6559eb1859d5cbcaeb4bf6bbb56486bb39fb5fc6
Meghan Urback <murback@lexmachina.com>
2016-11-01 15:22:02 -0700

    fix webpack config to output to static

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
