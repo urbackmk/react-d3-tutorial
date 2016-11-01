dd3f85da40884dd675c4db1ba7522c6f8a38b918
Meghan Urback <murback@lexmachina.com>
2016-11-01 16:11:56 -0700

    add basic data and render functions to H1BGraph component

--- src/components/H1BGraph/index.jsx
+++ src/components/H1BGraph/index.jsx
@@ -2,7 +2,54 @@ import React, { Component } from 'react';
 import d3 from 'd3';
 
 class H1BGraph extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rawData: []
+        };
+    }
+
+    componentWillMount() {
+        this.loadRawData();
+    }
+
+    loadRawData() {
+        let dateFormat = d3.time.format('%m/%d/%Y');
+
+        d3.csv(this.props.url)
+          .row((d) => {
+              if (!d['base salary']) {
+                  return null;
+              }
+
+              return {employer: d.employer,
+                      submit_date: dateFormat.parse(d['submit date']),
+                      start_date: dateFormat.parse(d['start date']),
+                      case_status: d['case status'],
+                      job_title: d['job title'],
+                      clean_job_title: d['job title'],
+                      base_salary: Number(d['base salary']),
+                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
+                      city: d.city,
+                      state: d.state};
+          })
+          .get((error, rows) => {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              }else{
+                  this.setState({rawData: rows});
+              }
+          });
+    }
+
     render() {
+        if (!this.state.rawData.length) {
+            return (
+                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+            );
+        }
         return (
             <div>
                 <svg>
