443ac0a74e3058ca431c31ada53f46e032ce54bc
Meghan Urback <murback@lexmachina.com>
2016-11-01 15:22:26 -0700

    add barebones H1BGraph component

---
+++ src/components/H1BGraph/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+class H1BGraph extends Component {
+    render() {
+        return (
+            <div>
+                <svg>
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default H1BGraph;
